52f60696b9d7b393d2e4f186385767f826221aae
aidenm1 <aidengmathew@gmail.com>
2025-11-17 20:56:49 -0800

    button routing in account success

--- app/(auth)/account_success/page.tsx
+++ app/(auth)/account_success/page.tsx
@@ -10,7 +10,7 @@ export default function AccountSuccess() {
   const router = useRouter();
 
   const handleViewSessions = () => {
-    router.push("/");
+    router.push("/sessions");
   };
 
   return (
