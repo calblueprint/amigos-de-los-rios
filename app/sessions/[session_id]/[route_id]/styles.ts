55cc1646c26cf4962e7d3d9d9d6137060404cd5f
Dhruva Bhat <dhruva.betkoppa@gmail.com>
2026-04-19 02:11:00 -0700

    [feat] implemented map embedding, created general structure with placeholder… (#29)
    * implemented map embedding, created general structure with placeholders for route points and team assignments
    
    implemented map embedding, created general structure with placeholders for route points and team assignments
    
    * converted px to rem and updated eslint run
    
    * changed colors
    
    * updated image type of icons
    
    * changed from img to Image and added alt
    
    * removing color legend for map embed and unused style component RouteType
    
    * removing unused styled components DotPurple Blue and Orange
    
    ---------
    
    Co-authored-by: bhavitaap <bhavita.peddi@gmail.com>

--- app/sessions/[session_id]/[route_id]/styles.ts
+++ app/sessions/[session_id]/[route_id]/styles.ts
@@ -38,11 +38,114 @@ export const BackLink = styled(Link)`
   }
 `;
 
-export const ContentContainer = styled.div`
+export const AllContent = styled.div`
+  display: inline-flex;
+  height: 79.25rem; /* 1268px */
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1.5rem; /* 24px */
   margin: 0 15%;
 `;
 
+export const ContentContainer = styled.div`
+  display: flex;
+  width: auto;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.5rem; /* 24px */
+`;
+
+export const LargeDotPurple = styled.div`
+  width: 2rem; /* 32px */
+  height: 2rem; /* 32px */
+  flex-shrink: 0;
+  border-radius: 1048575rem; /* 16777200px */
+  background: ${COLORS.purple};
+`;
+
+export const LargeDotBlue = styled.div`
+  width: 2rem; /* 32px */
+  height: 2rem; /* 32px */
+  flex-shrink: 0;
+  border-radius: 1048575rem; /* 16777200px */
+  background: ${COLORS.blue};
+`;
+
+export const LargeDotOrange = styled.div`
+  width: 2rem; /* 32px */
+  height: 2rem; /* 32px */
+  flex-shrink: 0;
+  border-radius: 1048575rem; /* 16777200px */
+  background: ${COLORS.orange};
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  height: auto;
+  padding: 1.5625rem; /* 25px */
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.5rem; /* 24px */
+  align-self: stretch;
+
+  border-radius: 0.625rem; /* 10px */
+  background: ${COLORS.adlr_white};
+  box-shadow:
+    0 0.0625rem 0.1875rem 0 rgba(0, 0, 0, 0.1),
+    /* 0 1px 3px 0 */ 0 0.0625rem 0.125rem -0.0625rem rgba(0, 0, 0, 0.1); /* 0 1px 2px -1px */
+  border: 0.0625rem solid ${COLORS.adlr_light_gray}; /* 1px */
+`;
+
+export const RouteValue = styled.div`
+  display: flex;
+  height: auto;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 1rem; /* 16px */
+  flex-shrink: 0;
+  align-self: stretch;
+`;
+
+export const RouteValueCard = styled.div`
+  display: flex;
+  height: auto;
+  align-items: flex-start;
+  gap: 0.75rem; /* 12px */
+`;
+
+export const RouteValueCardText = styled.div`
+  display: flex;
+  width: auto;
+  height: auto;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.25rem; /* 4px */
+`;
+
+export const RouteValueVar = styled.h1`
+  color: ${COLORS.adlr_property_gray};
+  font-family: "DM Sans";
+  font-size: 0.875rem; /* 14px */
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.3125rem; /* 21px */ /* 150% */
+  letter-spacing: -0.013125rem; /* -0.21px */
+`;
+
+export const RouteValueVarNum = styled.h1`
+  color: ${COLORS.black};
+  font-family: "DM Sans";
+  font-size: 1.125rem; /* 18px */
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.6875rem; /* 27px */ /* 150% */
+  letter-spacing: -0.016875rem; /* -0.27px */
+`;
+
 export const Header = styled.h1`
+  display: flex;
+  align-items: center;
+  align-self: stretch;
   font-family: ${Sans.style.fontFamily};
   font-size: 2.5rem;
   font-style: normal;
@@ -77,7 +180,7 @@ export const Tab = styled.div<{ $active?: boolean }>`
   cursor: pointer;
   transition: all 0.2s;
   border-radius: 0.3125rem;
-  border: 1px solid ${COLORS.adlr_light_gray};
+  border: 0.0625rem solid ${COLORS.adlr_light_gray}; /* 1px */
   margin-bottom: 1.25rem;
 
   @media (max-width: ${MOBILE_BREAKPOINT}) {
@@ -90,6 +193,151 @@ export const Tab = styled.div<{ $active?: boolean }>`
     background: ${props =>
       props.$active ? COLORS.adlr_light_gray : COLORS.adlr_light_gray};
   }
+`;
+
+export const TeamContainer = styled.div`
+  display: flex;
+  width: auto;
+  height: auto;
+  padding: 1.5625rem; /* 25px */
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.5rem; /* 24px */
+
+  border-radius: 0.625rem; /* 10px */
+  border: 0.0625rem solid ${COLORS.adlr_light_gray}; /* 1px */
+  background: ${COLORS.adlr_white};
+  box-shadow:
+    0 0.0625rem 0.1875rem 0 rgba(0, 0, 0, 0.1),
+    /* 0 1px 3px 0 */ 0 0.0625rem 0.125rem -0.0625rem rgba(0, 0, 0, 0.1); /* 0 1px 2px -1px */
+`;
+
+export const TeamAssignment = styled.h1`
+  color: ${COLORS.black};
+  font-family: "DM Sans";
+  font-size: 1.5rem; /* 24px */
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2.25rem; /* 36px */ /* 150% */
+  letter-spacing: -0.0225rem; /* -0.36px */
+`;
+
+export const TeamAssignmentCard = styled.div`
+  display: flex;
+  height: auto;
+  padding: 0.8125rem; /* 13px */
+  flex-direction: column;
+  align-items: flex-start;
+  flex-shrink: 0;
+  align-self: stretch;
+  border-radius: 0.625rem; /* 10px */
+  border: 0.0625rem solid ${COLORS.adlr_gray}; /* 1px */
+  background: ${COLORS.adlr_white};
+`;
+
+export const TeamAssignmentText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  flex: 1 0 0;
+`;
+
+export const TeamAssignmentName = styled.h1`
+  color: ${COLORS.adlr_black};
+  font-family: "DM Sans";
+  font-size: 1rem; /* 16px */
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5rem; /* 24px */
+  letter-spacing: -0.0195rem; /* -0.312px */
+`;
+
+export const TeamAssignmentRole = styled.h1`
+  color: ${COLORS.adlr_gray};
+  font-family: "DM Sans";
+  font-size: 0.875rem; /* 14px */
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.25rem; /* 20px */
+  letter-spacing: -0.009375rem; /* -0.15px */
+`;
+
+export const RouteContainer = styled.div`
+  display: flex;
+  height: auto;
+  padding: 1.5625rem; /* 25px */
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem; /* 16px */
+  align-self: stretch;
+
+  border-radius: 0.625rem; /* 10px */
+  border: 0.0625rem solid ${COLORS.adlr_light_gray}; /* 1px */
+  background: ${COLORS.adlr_white};
+  box-shadow:
+    0 0.0625rem 0.1875rem 0 rgba(0, 0, 0, 0.1),
+    /* 0 1px 3px 0 */ 0 0.0625rem 0.125rem -0.0625rem rgba(0, 0, 0, 0.1); /* 0 1px 2px -1px */
+`;
+export const RouteHeader = styled.div`
+  display: flex;
+  height: auto;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  align-self: stretch;
+`;
+
+export const RouteMap = styled.h1`
+  color: ${COLORS.black};
+  font-family: "DM Sans";
+  font-size: 1.25rem; /* 20px */
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.5625rem; /* 25px */ /* 125% */
+  letter-spacing: -0.01875rem; /* -0.3px */
+`;
+
+export const RouteHolder = styled.div`
+  display: flex;
+  height: auto;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.75rem; /* 12px */
+  flex-shrink: 0;
+  align-self: stretch;
+`;
+
+export const RoutePoints = styled.h1`
+  color: ${COLORS.black};
+  font-family: "DM Sans";
+  font-size: 1.125rem; /* 18px */
+  font-style: normal;
+  font-weight: 700;
+  line-height: 1.6875rem; /* 27px */ /* 150% */
+  letter-spacing: -0.016875rem; /* -0.27px */
+`;
+export const PropertiesHolder = styled.div`
+  display: flex;
+  height: auto;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.5rem; /* 8px */
+  flex-shrink: 0;
+  align-self: stretch;
+`;
+
+export const PropertiesCard = styled.div`
+  display: flex;
+  height: 4.4375rem; /* 71px */
+  padding: 0 0.75rem; /* 0 12px */
+  align-items: center;
+  gap: 0.75rem; /* 12px */
+  flex-shrink: 0;
+  align-self: stretch;
+  border-radius: 0.625rem; /* 10px */
+  border: 0.0625rem solid ${COLORS.adlr_light_gray}; /* 1px */
+  background: ${COLORS.adlr_white};
 `;
 
 export const PropertiesList = styled.div`
