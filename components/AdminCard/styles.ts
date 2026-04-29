72c515342c67548aaf17cc8541591b208efc24af
bhavitaap <144964319+bhavitaap@users.noreply.github.com>
2026-04-29 00:17:17 -0700

    Ami 55 add editability to the admin page (#50)
    * finished admin page
    
    * querying data from supabase
    
    * update styling
    
    * add box shadow
    
    * fixed code from pr review
    
    * add editability to the admin page
    
    * matched Prettier style
    
    * fixed styling
    
    * styling
    
    * styling
    
    * fixed AddCard Container styling
    
    * added hover to the trash icon
    
    * fixed code after pr review
    
    * removed full name field
    
    * adding menu bar and routing to admin page
    
    ---------
    
    Co-authored-by: Anh Vo <anhngocvanvo@berkeley.edu>

---
+++ components/AdminCard/styles.ts
@@ -0,0 +1,92 @@
+"use client";
+
+import styled from "styled-components";
+import COLORS from "@/styles/colors";
+
+export const CardContainer = styled.div`
+  display: flex;
+  height: 7.0625rem;
+  padding: 0 1rem;
+  justify-content: space-between;
+  align-items: center;
+  flex-shrink: 0;
+  align-self: stretch;
+  border-radius: 0.625rem;
+  margin-bottom: 1rem;
+  border: 1px solid ${COLORS.adlr_gray};
+`;
+
+export const LeftSection = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Avatar = styled.div`
+  width: 3.125rem;
+  height: 3.125rem;
+  border-radius: 1048575rem;
+  background-color: ${COLORS.adlr_blue};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  svg {
+    stroke: ${COLORS.adlr_white};
+  }
+`;
+
+export const AdminInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.15rem;
+`;
+
+export const AdminName = styled.p`
+  color: ${COLORS.adlr_black};
+  font-family: "DM Sans";
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.875rem; /* 150% */
+  letter-spacing: -0.01875rem;
+`;
+
+export const AdminEmail = styled.p`
+  color: ${COLORS.adlr_dark_gray};
+  font-family: "DM Sans";
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.5rem;
+  letter-spacing: -0.015rem;
+`;
+
+export const AdminAffiliation = styled.p`
+  color: ${COLORS.adlr_dark_gray};
+  font-family: "DM Sans";
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.3125rem;
+  letter-spacing: -0.01313rem;
+`;
+
+export const DeleteButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  color: ${COLORS.adlr_dark_gray};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.25rem;
+  border-radius: 0.25rem;
+  height: 1.25rem;
+  transition: opacity 0.2s;
+
+  &:hover {
+    color: ${COLORS.red};
+    opacity: 0.7;
+  }
+`;
